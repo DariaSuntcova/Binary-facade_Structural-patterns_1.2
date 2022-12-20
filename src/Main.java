4bf0b2ea986074df3c9eab6635924b428927bcf8
Daria Suntcova <batareyka23@gmail.com>
2022-12-20 14:15:03 +0300

    add BinOps

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("1000", "1010"));
+
+        System.out.println(bins.mult("1000", "1010"));
+    }
+}
